b6f0b1e5107b612d57b393dea1f4edabb4f4efed
Michal Zagrodzki <michalzagrodzki@gmail.com>
2020-04-28 20:14:40 +0200

    Refactor method for storing tokens

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,33 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import User from '../views/User.vue'
 
+/** 
+ * Add authentication token to sessionStorage.
+ * 
+ * Token is retrieved from uri path.
+ * */ 
+function addSessionToken (token, key, next) {
+  if (token) {
+    const decodedUriToken = decodeURIComponent(token)
+    sessionStorage.setItem(key, decodedUriToken)
+  }
+  return next();
+}
+
+/** 
+ * Check if route requires authentication.
+ * 
+ * Check if token is present in sessionStorage.
+ * */ 
+function isAuthenticated (match, next) {
+  const token = sessionStorage.getItem('token')
+  if (match && token !== null) {
+    return next()
+  } else {
+    return next('/')
+  }
+}
+
 Vue.use(VueRouter)
 
   const routes = [
@@ -14,7 +41,10 @@ Vue.use(VueRouter)
   {
     path: '/user',
     name: 'User',
-    component: User
+    component: User,
+    meta: {
+      requiresAuth: true
+    }
   },
   /* {
     path: '/about',
@@ -30,6 +60,12 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  addSessionToken(to.query.access, 'access', next);
+  addSessionToken(to.query.refresh, 'refresh', next);
+  isAuthenticated(to.matched.some(record => record.meta.requiresAuth), next);
 })
 
 export default router
